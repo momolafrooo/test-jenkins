48414c39a085f87cafe4063316db971cf982f0fd
momo <mouhacent@gmail.com>
2023-06-17 22:13:40 +0000

    initial commit

---
+++ src/main/java/com/groupeisi/mohamedfall/MohamedfallApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.mohamedfall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohamedfallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohamedfallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeisi/mohamedfall/MohamedfallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.mohamedfall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohamedfallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
