6e85956089b7836fed1055a1a62b917ee8a9a260
momo <mouhacent@gmail.com>
2023-06-17 23:56:06 +0000

    add sysout to MohamedfallApplication

--- src/main/java/com/groupeisi/mohamedfall/MohamedfallApplication.java
+++ src/main/java/com/groupeisi/mohamedfall/MohamedfallApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MohamedfallApplication {
 
 	public static void main(String[] args) {
+		System.out.println("SpringApplication start");
 		SpringApplication.run(MohamedfallApplication.class, args);
 	}
 
